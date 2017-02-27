237bdab06b71349dfd25b7647e6614e02f981741
David Max <davidkmax97@gmail.com>
2017-02-27 14:29:49 -0500

    Perimeter completed
    Final version of perimeter and angle data definitions.
    Plan for split is to have function that crawls for flase convex?,
    "cycles" list to make the angle the first in perim, look for conecting
    point for sub-perim, and split perim into two perims. Recurse this until
    perim-convex? is true for all perim

--- collisions.rkt
+++ collisions.rkt
@@ -1,113 +1,114 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname colion) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-; convex? : Posn Posm Posn -> Boolean
-; is the angle /_ posnA posnB posnC les than 180 going counter clockwise?
-; calculates by determining if the cross product BA X BC is positive in the Z
-; ... direction where BA and BC are vectors in 3 space with Z = 0
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname collisions) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;; an Angl is a (make-angl Posn Posn Posn)
+;;; - where the posns go in order of endpoint first line, center, and endpoint of second line
+;;;   and the angle is measured counter-clockwise
+(define-struct angl [point1 center point2])
 
-(define (convex? posnA posnB posnC)
-  (local [; disp-vec : Posn Posn -> Posn
-          ; makes displacement vector posn1 - posn2.
+;;; a Perimiter is a (make-perim LoA Bool)
+;;; - where Bool is whether the perimtere describes a convex shape
+;;; - and LoA is one of
+;;; - (list Angle Angle Angle)
+;;; - (cons Angle Perimiter)
+(define-struct perim [LoA convex?])
+
+; split-sprite : [List-of Points] -> [List-of [List-of Points]]
+; splits the sprite's vertecies into parts which are each convex
+#;
+(define (split-shape lop)
+  (local [(define perim (make-perimiter lop)) ; perimeter of shape
+   ....]))
+
+
+;;; make-perimeter : [List-of Posns] -> Perimeter
+;;; converts every vertex to an angle with the posn as the center
+(define (make-perimeter lop)
+  (local [;; format-lop : [List-of X] -> [List-of X]
+          ;; last element is added to the beginning and the first element is apendded to the end
+          ;; of the list
+          ;; - list is formated to represent rapping of points since list represent continuous object
+          (define (format-list lst)
+            (append (cons (last lst) lst) (list (first lst))))
+          ;; recursive-fun : [List-of Posns] -> Perimeter
+          ;; takes in fromated lop and replaces every point of object with angle
+          (define (make-loa lop)
+            (cond [(empty? (rest (rest lop))) empty]
+                  [else (cons (make-angl (first  lop)
+                                         (second lop)
+                                         (third  lop))
+                              (make-loa (rest lop)))]))
+          (define loa (make-loa (format-list lop)))] ; list of angles
+    (make-perim loa (andmap convex? loa))))
+
+
+(check-expect (make-perimeter (list (make-posn 0 0) (make-posn 0 2)
+                                    (make-posn 2 2) (make-posn 1 1)
+                                    (make-posn 2 0)))                 ; shape that looks like [<
+              (make-perim (list (make-angl (make-posn 2 0) (make-posn 0 0) (make-posn 0 2))
+                                (make-angl (make-posn 0 0) (make-posn 0 2) (make-posn 2 2))
+                                (make-angl (make-posn 0 2) (make-posn 2 2) (make-posn 1 1))
+                                (make-angl (make-posn 2 2) (make-posn 1 1) (make-posn 2 0))
+                                (make-angl (make-posn 1 1) (make-posn 2 0) (make-posn 0 0)))
+                          #false))
+(check-expect (make-perimeter (list (make-posn 0 0) (make-posn 0 1)
+                                    (make-posn 1 1) (make-posn 1 0))) ; square
+              (make-perim (list (make-angl (make-posn 1 0) (make-posn 0 0) (make-posn 0 1))
+                                (make-angl (make-posn 0 0) (make-posn 0 1) (make-posn 1 1))
+                                (make-angl (make-posn 0 1) (make-posn 1 1) (make-posn 1 0))
+                                (make-angl (make-posn 1 1) (make-posn 1 0) (make-posn 0 0)))
+                          #true))
+
+;;; last : [List-of X] -> X
+;;; last element of list
+(define (last lst)
+  (cond [(empty? lst)        (error "last expected a non-empty list but recieved empty")]
+        [(empty? (rest lst)) (first lst)]
+        [else                (last (rest lst))]))
+
+(check-expect (last (list 2 5 6)) 6)
+(check-expect (last (list 3))     3)
+
+;;; convex? : Angl -> Boolean
+;;; is the angle less than 180?
+;;; calculates by determining if the cross product BA X BC is positive in the Z
+;;; ... direction where BA and BC are vectors in 3 space with Z = 0
+
+(define (convex? angl)
+  (local [;; disp-vec : Posn Posn -> Posn
+          ;; makes displacement vector from posn2 to posn1.
           (define (disp-vec posn1 posn2) (make-posn (- (posn-x posn1)
                                                        (posn-x posn2))
                                                     (- (posn-y posn1)
                                                        (posn-y posn2))))
-          (define vecBA (disp-vec posnA posnB))
-          (define vecBC (disp-vec posnC posnB))
-          (define vecBA_X_vecBC (- (* (posn-x vecBA) (posn-y vecBC))
-                                   (* (posn-y vecBA) (posn-x vecBC))))]
+          (define vecBA (disp-vec (angl-point1 angl) (angl-center angl)))
+          (define vecBC (disp-vec (angl-point2 angl) (angl-center angl)))
+          (define vecBA_X_vecBC (- (* (posn-x vecBA) (posn-y vecBC))    ; the z component of cross
+                                   (* (posn-y vecBA) (posn-x vecBC))))] ; product 
     (cond [(zero?     vecBA_X_vecBC) (error "points are collinear")]
           [(positive? vecBA_X_vecBC) #t]
           [(negative? vecBA_X_vecBC) #f]
-          [else (error "something went wrong...")])))
+          [else (error "something went wrong with cconvex?...")])))
 
-(check-expect (convex? (make-posn 1 0)
-                       (make-posn 0 0)
-                       (make-posn 0 1))
+(check-expect (convex? (make-angl (make-posn 1 0)
+                                  (make-posn 0 0)
+                                  (make-posn 0 1)))
               #t)
-(check-expect (convex? (make-posn -3 -1)
-                       (make-posn 0 0)
-                       (make-posn 2 -1))
+(check-expect (convex? (make-angl (make-posn -3 -1)
+                                  (make-posn 0 0)
+                                  (make-posn 2 -1)))
               #t)
-(check-expect (convex? (make-posn 1 0)
-                       (make-posn 0 0)
-                       (make-posn 2 -1))
+(check-expect (convex? (make-angl (make-posn 1 0)
+                                  (make-posn 0 0)
+                                  (make-posn 2 -1)))
               #f)
-(check-expect (convex? (make-posn 1 0)
-                       (make-posn 0 0)
-                       (make-posn 0 -1))
+(check-expect (convex? (make-angl (make-posn 1 0)
+                                  (make-posn 0 0)
+                                  (make-posn 0 -1)))
               #f)
-(check-error  (convex? (make-posn -1 0)
-                       (make-posn 1 4)
-                       (make-posn 5 12))
+(check-error  (convex? (make-angl (make-posn -1 0)
+                                  (make-posn 1 4)
+                                  (make-posn 5 12)))
               "points are collinear")
 
-; an Line is a (list Number Number Number)
-; - where (list a b c) represents the equation aX + bY = c
-(define line-x first)
-(define line-y second)
-(define line-c third)
-
-; split-sprite : [List-of Points] -> [List-of [List-of Points]]   (use point itself that is use first convex point
-; splits the sprite's vertecies into parts which are each ... :'(
-#;(define (split-shape lop)
-    (local [(define lop-total (append lop
-                                      (list (first lop)
-                                            (second lop))))
-            (define (recusion lop)
-              (cond [(< (length lop) 3) empty]
-                    [(convex? (first lop) (second lop) (third lop))
-                     (cons (first lop) (recursion))]
-                    [else (...
-
-; point-of-intersection : Line Line -> Posn   (not neccessary anymore)
-; the point of intersection between the line AB and CD
-; computes via solution of matrix of two equations. Assumes that the two lines are not parallel
-(define (point-of-intersection equation1 equation2)
-  (cond [(and (zero? (line-y equation1))
-              (zero? (line-x equation2)))
-         (make-posn (/ (line-c equation1) (line-x equation1))
-                    (/ (line-c equation2) (line-y equation2)))]
-        [(zero? (line-y equation1))
-         (point-of-intersection equation1 
-                               (sub-line equation1
-                                         (/ (line-x equation2) (line-x equation1))
-                                         equation2))]
-        [(zero? (line-y equation2))
-         (point-of-intersection equation2
-                                equation1)]
-        [else
-         (point-of-intersection (sub-line equation2
-                                          (/(line-y equation1) (line-y equation2))
-                                          equation1)
-                                equation2)]))
-(check-expect (point-of-intersection (list 2 3 7)
-                                     (list 3 5 11))
-              (make-posn 2 1))
-
-
-; sub-line: Line Number Line -> Line
-; subtracts the first line from the second line with a constant multiple
-(define (sub-line sub-l multiplier base-l)
-  (list (- (line-x base-l)
-           (* (line-x sub-l) multiplier))
-        (- (line-y base-l)
-           (* (line-y sub-l) multiplier))
-        (- (line-c base-l)
-           (* (line-c sub-l) multiplier))))
-
-(check-expect (sub-line (list 1 2 3) 2 (list 9 9 9))
-              (list                          7 5 3))
-
-; make-equation : Posn Posn -> Line
-; eqation of line that passes through two posns
-(define (make-equation posn1 posn2)
-  (local [(define a (- (posn-y posn1) (posn-y posn2)))
-          (define b (- (posn-x posn1) (posn-x posn2)))
-          (define c (+ (* a (posn-x posn1)) (* b (posn-y posn1))))]
-    (list a b c)))
-
-(check-expect (make-equation (make-posn 0 3) (make-posn -4 -5))
-              (list 8 4 12))
 
